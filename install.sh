7d9286b9ef3541112b5f7e0de79a27082ab63701
LifelagCheats <179240443+LifelagCheats@users.noreply.github.com>
2025-04-06 13:47:40 -0400

    Completely refractored the code for python3. added significant changes that increased readability appart from that, i also added newer, simpler and more feature-rich modules that sped up the code review and workflow some of those modules include: - requests - rich - stem - argparse and for parsing Command-Line Arguments, i included argparse and replaced sys, as it allowed for easier control over options.

---
+++ install.sh
@@ -0,0 +1,50 @@
+clear
+echo "******* TorPhantom Installer ********"
+echo ""
+if [ -f /etc/os-release ]; then
+    source /etc/os-release
+    case "$ID" in
+        debian|ubuntu|linuxmint)
+            echo ""
+            echo "=====> Installing macchanger "
+            sudo apt-get install macchanger -y -qq
+            echo "=====> Installing tor bundle "
+            sudo apt-get install tor -y -qq
+            echo "=====> Done "
+            echo "=====> Open terminal and type 'python torphantom.py' for usage "
+            ;;
+        fedora|rhel|centos|rocky|alma)
+            echo "=====> Installing macchanger "
+            sudo yum -y -q macchanger
+            echo "=====> Installing tor bundle "
+            sudo yum -y -q tor
+            echo "=====> Done "
+            echo "=====> Open terminal and type 'python torphantom.py' for usage "
+            ;;
+        arch|manjaro)
+            echo "=====> Installing macchanger "
+            sudo pacman -S --quiet --noconfirm --needed macchanger
+            echo "=====> Installing tor bundle "
+            sudo pacman -S --quiet --noconfirm --needed tor
+            echo "=====> Done "
+            echo "=====> Open terminal and type 'python torphantom.py' for usage "
+            ;;
+        *)
+            echo ""
+            echo "=====> Installing macchanger "
+            git clone https://github.com/InjectionSoftwareDevelopment/macchanger
+            cd macchanger
+            sudo chmod +x install.sh
+            ./install.sh
+            cd ../
+            rm -rf macchanger
+            echo "=====> Installing tor bundle "
+            sudo port install tor
+            sudo mv /opt/local/etc/tor/torrc.sample /opt/local/etc/tor/torrc
+            echo "=====> Done "
+            echo "=====> Open terminal and type 'python torphantom' for usage "
+            ;;
+    esac
+else
+    echo "Unable to detect the distribution family."
+fi
